4ad8e16d0a39b601e32e4d0c7787520f21bdfbb4
giglaulala <gigla.titina777@gmail.com>
2025-03-17 23:38:45 +0400

    feat: add welcome screen

--- app/welcome.jsx
+++ app/welcome.jsx
@@ -1,15 +1,96 @@
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Pressable,
+} from "react-native";
 import React from "react";
 import ScreenWrapper from "../components/ScreenWrapper";
+import { hp, wp } from "../helpers/common";
+import { theme } from "../constants/theme";
+import Button from "../components/Button";
 
 const Welcome = () => {
   return (
-    <ScreenWrapper>
-      <Text>Welcome</Text>
+    <ScreenWrapper bg="white">
+      <StatusBar style="dark" />
+      <View style={styles.container}>
+        <Image
+          resizeMode="contain"
+          style={styles.welcomeImage}
+          source={require("../assets/images/welcome.png")}
+        />
+        <View style={{ gap: 20 }}>
+          <Text style={styles.title}>LinkUp!</Text>
+          <Text style={styles.punchline}>
+            Where every though finds a home and every image tells a story!
+          </Text>
+        </View>
+
+        <View style={styles.footer}>
+          <Button
+            title="Getting Started"
+            buttonStyle={{ marginHorizontal: wp(3) }}
+            onPress={() => {}}
+          />
+          <View style={styles.bottomTextContainer}>
+            <Text style={styles.loginText}>Already have an account!</Text>
+            <Pressable>
+              <Text
+                style={[styles.loginText, { color: theme.colors.primaryDark }]}
+              >
+                Login
+              </Text>
+            </Pressable>
+          </View>
+        </View>
+      </View>
     </ScreenWrapper>
   );
 };
 
 export default Welcome;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "space-around",
+    backgroundColor: "white",
+    paddingHorizontal: wp(4),
+  },
+  welcomeImage: {
+    height: hp(30),
+    width: wp(100),
+    alignSelf: "center",
+  },
+  title: {
+    color: theme.colors.text,
+    fontSize: hp(4),
+    textAlign: "center",
+    fontWeight: theme.fonts.extraBold,
+  },
+  punchline: {
+    textAlign: "center",
+    paddingHorizontal: wp(10),
+    fontSize: hp(1.7),
+    color: theme.colors.text,
+  },
+  footer: {
+    gap: 30,
+    width: "100%",
+  },
+  bottomTextContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 5,
+  },
+  loginText: {
+    textAlign: "center",
+    color: theme.colors.text,
+    fontSize: hp(1.6),
+  },
+});
