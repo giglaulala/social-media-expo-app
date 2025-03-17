d7e4204dedabdf693367b955d031b789b3635597
giglaulala <gigla.titina777@gmail.com>
2025-03-18 01:12:43 +0400

    feat: add login page

--- app/login.jsx
+++ app/login.jsx
@@ -1,16 +1,68 @@
-import { StatusBar, StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { Pressable, StatusBar, StyleSheet, Text, View } from "react-native";
+import React, { useRef, useState } from "react";
 import ScreenWrapper from "../components/ScreenWrapper";
 import Icon from "../assets/icons";
 import BackButton from "../components/BackButton";
 import { useRouter } from "expo-router";
+import { theme } from "../constants/theme";
+import { hp, wp } from "../helpers/common";
+import Input from "../components/Input";
+import Button from "../components/Button";
 const Login = () => {
   const router = useRouter();
+  const emailRef = useRef("");
+  const passwordRef = useRef("");
+  const [loading, setLoading] = useState(false);
+  const onSubmit = async () => {
+    if (!emailRef.current || !passwordRef.current) {
+      Alert.alert("Login", "please fill all fields!");
+      return;
+    }
+  };
   return (
-    <ScreenWrapper>
+    <ScreenWrapper bg="white">
       <StatusBar style="dark" />
       <View style={styles.container}>
         <BackButton router={router} />
+        <View>
+          <Text style={styles.welcomeText}>Hey,</Text>
+          <Text style={styles.welcomeText}>Welcome Back</Text>
+        </View>
+
+        <View style={styles.form}>
+          <Text style={{ fontSize: hp(1.5), color: theme.colors.text }}>
+            Please login to continue
+          </Text>
+          <Input
+            icon={<Icon name="mail" size={26} strokeWidth={1.6} />}
+            placeholder="Enter your email"
+            onChangeText={(value) => (emailRef.current = value)}
+          />
+          <Input
+            icon={<Icon name="lock" size={26} strokeWidth={1.6} />}
+            placeholder="Enter your password"
+            secureTextEntry
+            onChangeText={(value) => (passwordRef.current = value)}
+          />
+          <Text style={styles.forgotPassword}>Forgot Password?</Text>
+          <Button title={"Login"} loading={loading} onPress={onSubmit} />
+        </View>
+        <View style={styles.footer}>
+          <Text style={styles.footerText}>Don't have an account?</Text>
+          <Pressable>
+            <Text
+              style={[
+                styles.footerText,
+                {
+                  color: theme.colors.primaryDark,
+                  fontWeight: theme.fonts.semibold,
+                },
+              ]}
+            >
+              Sign up
+            </Text>
+          </Pressable>
+        </View>
       </View>
     </ScreenWrapper>
   );
@@ -18,4 +70,34 @@ const Login = () => {
 
 export default Login;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    gap: 45,
+    paddingHorizontal: wp(5),
+  },
+  welcomeText: {
+    fontSize: hp(4),
+    fontWeight: theme.fonts.bold,
+    color: theme.colors.text,
+  },
+  form: {
+    gap: 25,
+  },
+  forgotPassword: {
+    textAlign: "right",
+    fontWeight: theme.fonts.semibold,
+    color: theme.colors.text,
+  },
+  footer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 5,
+  },
+  footerText: {
+    textAlign: "center",
+    color: theme.colors.text,
+    fontSize: hp(1.6),
+  },
+});
