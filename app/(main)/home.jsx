374bdf39527c995bc8c9026c531c9fc9b0f422ef
giglaulala <gigla.titina777@gmail.com>
2025-03-19 22:45:41 +0400

    feat: add homepage header

--- app/(main)/home.jsx
+++ app/(main)/home.jsx
@@ -1,13 +1,49 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 import React from "react";
 import ScreenWrapper from "../../components/ScreenWrapper";
 import Button from "../../components/Button";
-import { router } from "expo-router";
+import { router, useRouter } from "expo-router";
+import { hp, wp } from "../../helpers/common";
+import { theme } from "../../constants/theme";
+import Icon from "../../assets/icons";
+import Avatar from "../../components/Avatar";
 
 const Home = () => {
+  const router = useRouter();
   return (
-    <ScreenWrapper>
-      <Text>Home</Text>
+    <ScreenWrapper bg={"white"}>
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <Text style={styles.title}>LinkUp</Text>
+          <View style={styles.icons}>
+            <Pressable onPress={() => router.push("notifications")}>
+              <Icon
+                name="heart"
+                size={hp(3.2)}
+                strokeWidth={2}
+                color={theme.colors.text}
+              />
+            </Pressable>
+            <Pressable onPress={() => router.push("newPost")}>
+              <Icon
+                name="plus"
+                size={hp(3.2)}
+                strokeWidth={2}
+                color={theme.colors.text}
+              />
+            </Pressable>
+            <Pressable onPress={() => router.push("profile")}>
+              <Avatar
+                uri={require("../../assets/images/defaultUser.png")}
+                size={hp(4.3)}
+                rounded={theme.fonts.radius.sm}
+                style={{ borderWidth: 2 }}
+              />
+            </Pressable>
+          </View>
+        </View>
+      </View>
+
       <Button title="Logout" onPress={() => router.back()} />
     </ScreenWrapper>
   );
@@ -15,4 +51,59 @@ const Home = () => {
 
 export default Home;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  header: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginBottom: 10,
+    marginHorizontal: wp(4),
+  },
+  title: {
+    color: theme.colors.text,
+    fontSize: hp(3.2),
+    fontWeight: theme.fonts.bold,
+  },
+  avatarImage: {
+    height: hp(4.3),
+    width: hp(4.3),
+    borderRadius: theme.fonts.radius.sm,
+    borderCurve: "continuous",
+    borderColor: theme.colors.gray,
+    borderWidth: 3,
+  },
+  icons: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 18,
+  },
+  listStyle: {
+    paddingTop: 20,
+    paddingHorizontal: wp(4),
+  },
+  noPosts: {
+    fontSize: hp(2),
+    textAlign: "center",
+    color: theme.colors.text,
+  },
+  pill: {
+    position: "absolute",
+    right: -10,
+    top: -4,
+    height: hp(2.2),
+    width: hp(2.2),
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 20,
+    backgroundColor: theme.colors.roseLight,
+  },
+  pillText: {
+    color: "white",
+    fontSize: hp(1.2),
+    fontWeight: theme.fonts.bold,
+  },
+});
